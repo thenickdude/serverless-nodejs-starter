997af5b1c2f71801b270125f79ebf8d6c42ae947
Nicholas Sherlock <n.sherlock@gmail.com>
2018-02-06 12:31:30 +1300

    Move response generation to external package

--- handler.js
+++ handler.js
@@ -1,16 +1,7 @@
-export const hello = async (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Go Serverless v1.0! ${(await message({ time: 1, copy: 'Your function executed successfully!'}))}`,
-    }),
-  };
+const generateResponse = require("serverless-nodejs-starter-dep");
 
+export const hello = async (event, context, callback) => {
+  const response = await generateResponse();
+  
   callback(null, response);
 };
-
-const message = ({ time, ...rest }) => new Promise((resolve, reject) => 
-  setTimeout(() => {
-    resolve(`${rest.copy} (with a delay)`);
-  }, time * 1000)
-);
